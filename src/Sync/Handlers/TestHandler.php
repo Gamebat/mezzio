4acbd805732ec568a70dfa241766cc8a2594422d
Danya <malashnekov.danil@gmail.com>
2022-12-19 09:50:17 +0200

    Выводится имена и email

--- src/Sync/Handlers/TestHandler.php
+++ src/Sync/Handlers/TestHandler.php
@@ -35,11 +35,11 @@ class TestHandler implements RequestHandlerInterface
     {
         $clientId = "9c59de12-6982-4761-8967-c770ff9d544f";
         $clientSecret = "iwMJZLYZHrU7FUSbg0wHWSmkO3psJNGej7hVnwmGk2Djwh1DjDvV1s7tlgwdf4vB";
-        $redirectUri = "https://ee08-173-233-147-68.eu.ngrok.io/test";
+        $redirectUri = "https://ef90-173-233-147-68.eu.ngrok.io/test";
 
         $apiClient = new AmoCRMApiClient($clientId, $clientSecret, $redirectUri);
 
-        if (trim(file_get_contents('accessToken.json')) == false) {
+        if (!file_get_contents('accessToken.json')) {
             if (isset($_GET['referer'])) {
                 $apiClient->setAccountBaseDomain($_GET['referer']);
             }
@@ -114,90 +114,32 @@ class TestHandler implements RequestHandlerInterface
                 ->setAccessToken($token)
                 ->setAccountBaseDomain($token->getResourceOwnerId());
 
-            /*$field = $apiClient->contacts()->get()->pluck('name');
-            foreach ($field as $id => $item)
-            {
-                $contacts[$id]['name'] = $item;
-            }*/
 
-            $field2 = $apiClient->contacts()->get()->pluck('custom_fields_values');
-            $collection = $apiClient->contacts()->get();
-
-            $result = [];
-
-            foreach ($collection as $id => $contact) {
-                $result[$id]['name'] = $contact->getName();
-            }
-
-            foreach ($collection as $id => $contact){
-                $field = $contact -> getCustomFieldsValues() -> getBy('field_code','EMAIL');
-
-                /*if ($field != null){
-
-                    $result[$id]['field'] = $field -> getValues();
-                }*/
-                if ($field != null)
-                {
-
-                    $email = $field -> getValues();
-                    foreach ($email as $key => $value)
-                    {
-                        foreach ($key[$value] as $ss => $item)
-                        {
-                            $result[]['test'] = $item;
-                            if ($key === 'value' || 1 == 1){
-                                $result[$id]['email'] = $item;
-                            }
-                        }
-                    }
-
-                }
-
-            }
-
-
-            /*foreach ($field2 as $id => $item)
-            {
-                $contacts[$id]['fields'] = $item;
-            }*/
-
-            //На крайний случай
-
-            /*$field = $apiClient->contacts()->get()->pluck('name');
-            foreach ($field as $id => $item)
-            {
-                $result[$id]['name'] = $item;
-            }
-            $field2 = $apiClient->contacts()->get()->pluck('custom_fields_values');
-            $find = 0;
-            foreach ($field2 as $id => $item)
-            {
-                //$contacts[$id]['first'] = $item;
-                foreach ($item[0] as $key => $value)
-                {
-                    if ($value === 'Email')
-                    {
-                        $find = 1;
-                    }
-                }
-                if ($find === 1)
-                {
-                    foreach ($item[0]['values'] as $key => $value)
-                    {
-                        foreach ($value as $key => $value)
-                        {
-                            if ($key === 'value')
-                            {
-                                $result[$id]['email'] = $value;
-                            }
-
-                        }
-                    }
-                }
-            }*/
-            return $result;
         }
-        return $contacts;
+        $collection = $apiClient->contacts()->get();
+
+        $result = [];
+
+        foreach ($collection as $id => $contact) {
+            $result[$id]['name'] = $contact->getName();
+        }
+
+        foreach ($collection as $id => $contact){
+            $field = $contact -> getCustomFieldsValues() -> getBy('field_code','EMAIL');
+
+            if ($field != null)
+            {
+                $email = $field -> getValues();
+                foreach ($email as $value)
+                {
+                    $result[$id]['emails'][] = $value->getValue();
+                }
+
+            }
+
+        }
+
+        return $result;
     }
 
     public function saveToken($array): void
