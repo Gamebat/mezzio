56c3e2709afd2eef8c353bdd4f9430b4f9ff57c6
Danya <malashnekov.danil@gmail.com>
2022-12-29 16:39:43 +0300

    Вывод времени в консоль с помощью команды how-time

--- src/Sync/Laminas/MyCommand.php
+++ src/Sync/Laminas/MyCommand.php
@@ -4,11 +4,17 @@ namespace Sync\Laminas;
 
 use Carbon\Carbon;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
 
 class MyCommand extends Command
 {
-    public function howTime(){
-        printf("Now: %s", Carbon::now());
-    }
+    protected static $defaultDescription = 'Show current time';
 
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $date = Carbon::now()->format('H:i (m.Y)');
+        $output->writeln("Now time: ". $date);
+        return Command::SUCCESS;
+    }
 }
